a33ec05cb5f5c6f37334207a3ba0e51cec68e4d3
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-10-19 11:18:16 +0200

    19.10.2023

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class21-materials/objects-zeo/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,18 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {}
+
+stopwatch.color = 'black'
+stopwatch.shape = 'round'
+stopwatch.material = 'plastic'
+stopwatch.size = 'round'
+
+stopwatch.start = function (){
+    console.log('starting')
+}
+stopwatch.stop = function (){
+    console.log('stoping')
+}
+stopwatch.restartstart = function (){
+    console.log('restarting')
+}
