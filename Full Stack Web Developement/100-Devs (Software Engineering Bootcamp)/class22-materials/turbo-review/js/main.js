4332e16117d8c8913be725aa44f6aa855cb24d61
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-10-23 16:16:57 +0200

    23.10.23

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class22-materials/turbo-review/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class22-materials/turbo-review/js/main.js
@@ -4,12 +4,12 @@
     favDrink = favDrink.trim()
     // console.log(favDrink);
 //Declare a variable, assign it a string of multiple words, and check to see if one of the words is "apple".
-    let words = 'Hey everyone, Sour here. I like apples'
-    if(words.search('apples') !== -1){
-        console.log('yes')
-    } else {
-        console.log('no')
-    }
+    // let words = 'Hey everyone, Sour here. I like apples'
+    // if(words.search('apples') !== -1){
+    //     console.log('yes')
+    // } else {
+    //     console.log('no')
+    // }
     
 
 // *Functions*
@@ -29,8 +29,7 @@
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if the player won a game of rock paper scissors against a bot using the above function
     function checkWin (playerChoice){
-        let botChoice = rockPaperScissors()
-        
+        let botChoice = rockPaperScissors()        
         if(
             (playerChoice === 'rock' && botChoice === 'scissors') || 
             (playerChoice === 'paper' && botChoice === 'rock') ||
@@ -43,15 +42,18 @@
                 {
                 console.log('You lose')
             }
-            }    
+                }    
     }
-    checkWin('rock')
+    // checkWin('rock')
     
 
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
-    // function rockPaperScissors3 (){
-        
-    // }
+    function playGameXTimes (array){
+        array.forEach( (choice) => checkWin(choice) )
+    }
+    playGameXTimes(['rock', 'paper', 'scissors'])
+
+
 
 
