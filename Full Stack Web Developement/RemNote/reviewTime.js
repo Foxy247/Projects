321c099e1cf019b7217f3c793abd1b9ce4191c88
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-10-24 15:41:44 +0200

    24

--- Full Stack Web Developement/RemNote/reviewTime.js
+++ Full Stack Web Developement/RemNote/reviewTime.js
@@ -2,3 +2,12 @@ let numbers = [1,2,5,9,4,8]
 
 let evens = (arr) => arr.filter((n) => n % 2 === 0)
 
+
+
+// Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function onlyEvens (nums){
+    let evens = []
+
+    nums.forEach()
+}
