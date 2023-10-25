a36919b51d4774f791b9cf40277435e5b4808e1f
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-10-25 16:34:33 +0200

    25.10

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class22-materials/objects-lost-galaxy/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,35 @@
 //Create a mouse object that has four properties and three methods
+
+    // let mouse = {
+    //     form: 'round'
+    //     color: 'black'
+    //     brand: 'glorious'
+    //     buttons: 5
+    //     function click(){
+    //         console.log('clicking');
+    //     }
+    //     function run(){
+    //         console.log();
+    //     }
+    //     function scroll(){
+    //         console.log();
+    //     }
+    // }
+
+    let mouse = {}
+
+    mouse.form = 'round'
+    mouse.color = 'black'
+    mouse.click = function(){console.log('click');}
+
+    function MakeCar(carMake,carModel,carColor,numOfDoors){
+        this.make = carMake
+        this.model = carModel
+        this.color = carColor
+        this.doors = numOfDoors
+        this.honk = function(){
+            alert('BEEP BEEP FUCKER')
+        }
+    }
+
+    let hondaCivic = new MakeCar('Honda','Civic','Silver', 4)
--- Full Stack Web Developement/RemNote/reviewTime.js
+++ Full Stack Web Developement/RemNote/reviewTime.js
@@ -6,8 +6,3 @@ let evens = (arr) => arr.filter((n) => n % 2 === 0)
 
 // Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
 
-function onlyEvens (nums){
-    let evens = []
-
-    nums.forEach()
-}
