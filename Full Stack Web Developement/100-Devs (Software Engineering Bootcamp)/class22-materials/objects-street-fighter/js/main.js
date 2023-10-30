c33db272a74e0c1787194bdfb42d709e7e5675e2
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-10-30 19:38:24 +0100

    30.10

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class22-materials/objects-street-fighter/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,18 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighterFactory(punch,kick,taunt,specialMove){
+    this.punch = punch 
+    this.kick = kick
+    this.catchPhrase = catchPhrase
+    this.specialMove = specialMove
+    this.taunt = function (){
+        console.log('fight');
+    }
+    this.dodge = function dodge(){
+        console.log('dodge');
+    }
+    this.counter(){
+        console.log('counter');
+    }
+} 
+
