3f621c7b129e1826fd2f0cb806222fbbed61b5e8
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-11-04 10:37:34 +0100

    4.11

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class23-materials/space-review/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class23-materials/space-review/js/main.js
@@ -1,12 +1,20 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+    nums = [2,3,5,23,6]
 
+    nums = nums.reduce((acc,c) => acc + c)
+    // alert(nums)
+    
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
-
+    function squared(nums){
+        Math.pow(nums,2)
+    }
 //Create a function that takes string
 //Print the reverse of that string to the console
-
+    function reverse(str){
+        
+    }
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
