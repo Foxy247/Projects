8d9b75d4ae95f91dec1d23a4fd08fbb5cead1d89
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-11-08 22:38:09 +0100

    8.11

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,5 +1,28 @@
 //Create a Tony Hawk Pro Skater constructor that makes skating game characters with 4 properties and 3 methods
 
-function MakeSkaterCharacter () {
-    
+function MakeSkaterCharacter (charName,skill,hight) {
+    this.name = charName
+    this.skill = skill 
+    this.hight = hight
+    this.start = () => console.log('start');
+    this.stop = () => console.log('stop');
+    this.jump = () => console.log('jump');
 } 
+
+let kareemCampbell = new MakeSkaterCharacter('')
+
+function Factory (vehicle,ability){
+    this.vehicle = vehicle
+    this.minerals = minerals
+    this.gas = gas
+    this.supply = supply
+    this.hitpoints = hitpoints
+    this.range = range
+    this.move 
+    this.stop 
+    this.holdPosition
+    this.attackMove
+    this.specialAbility = () => console.log(`${}`); 
+}
+
+let siegeTank = new Factory ('siegeTank', 150, 125, 3, 175)
