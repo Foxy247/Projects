3c47a24a63843f1167741f18c7242ce79bd26fa2
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-11-08 16:09:33 +0100

    8.11

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class23-materials/objects-lost-galaxy/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,15 @@
 //Create a dog object that has four properties and three methods
+
+let dog = {
+    name: 'blacky',
+    color: 'black',
+    age: 11,
+    owner: 'Fanni',
+}
+
+dog.bark = function(){console.log('woof')}
+dog.walk = () => console.log('lets go')
+dog.eat = () => console.log('nom nom')
+
+
+     
