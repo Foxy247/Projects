df96c017921432e2925e357f6bc14db92998e1f3
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-12-04 20:44:24 +0100

    4

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-constructor-fun/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-constructor-fun/js/main.js
@@ -1,3 +1,10 @@
 //Create a constructor with 4 properties and 3 methods
 
-function PizzaFactory (pizzaMake,pizzaSize,pizzaName,pizzaToppings)
+function PizzaFactory (size,name,toppings){
+    this.PizzaSize = size
+    this.pizzaName = name
+    this.
+
+    this.pizzaToppings = toppings
+}
+
