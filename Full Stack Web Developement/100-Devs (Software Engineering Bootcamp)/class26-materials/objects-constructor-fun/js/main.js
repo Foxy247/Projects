5ffa2139fede6ee81c5cf8f36f6adbcb17411355
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-12-05 16:59:55 +0100

    5

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-constructor-fun/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-constructor-fun/js/main.js
@@ -1,10 +1,20 @@
 //Create a constructor with 4 properties and 3 methods
 
-function PizzaFactory (size,name,toppings){
-    this.PizzaSize = size
+function PizzaFactory (name,size,sauce,toppings){
     this.pizzaName = name
-    this.
-
+    this.pizzaSize = size
+    this.pizzaSauce = sauce 
     this.pizzaToppings = toppings
+
+    this.estimatedDeliveryTime = function (){
+        console.log('Calculating ...');
+    }
+    this.burnMouth = function(){
+        console.log('aaaaah');
+    }
+    this.frisbee = function(){
+        console.log('Yeeet');
+    }
 }
 
+let 
