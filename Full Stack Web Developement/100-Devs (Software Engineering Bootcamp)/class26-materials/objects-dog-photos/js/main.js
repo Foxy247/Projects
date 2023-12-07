1c8edc60e185d9d69a00c0faef9bf82c20c70a94
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-12-07 16:40:27 +0100

    7

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-dog-photos/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,12 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json()) // make sure the data is json
+    .then(data => {
+      console.log(data)     // what ever we get back pass through the parameter of data
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+
+// how do I plug this into the dom?
