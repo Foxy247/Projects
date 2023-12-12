fd8a45fba5aa71f9ad581dcb9bbbae7c8c61e49f
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-12-12 18:36:43 +0100

    12

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-shots/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-shots/js/main.js
@@ -4,11 +4,16 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data.strDrink)
-      document.querySelector('img').src = 
+      document.querySelector('img').src  
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
 
+    console.log(); 
+
+    {{
+      console.log(2*4)
+    }}
+
     
-    let array = [1,2,3]
--- Full Stack Web Developement/Own Projects/Coffe Tracker/script.js
+++
@@ -1,3 +0,0 @@
-let displayCup = document.querySelector('.cups total')
-let addCup = document.querySelector('.add-cup')
-let removeCup = document.querySelector('.remove-cup')
