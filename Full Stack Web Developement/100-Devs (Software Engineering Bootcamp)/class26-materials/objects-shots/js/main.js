2494c8a13152e88e4b00f78fcd97b63de71db94b
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-12-15 18:03:27 +0100

    15

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-shots/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-shots/js/main.js
@@ -1,4 +1,9 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+document.querySelector("button").addEventListener("click", getDrink);
+
+function getDrink() {
+  let drink = document.querySelector("input").value;
+}
 
 fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
   .then((res) => res.json()) // parse response as JSON
