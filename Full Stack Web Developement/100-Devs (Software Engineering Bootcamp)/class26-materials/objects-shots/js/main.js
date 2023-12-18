e924857432106ba23b1513637e5a2f29156f4a13
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-12-18 17:46:55 +0100

    18

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-shots/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-shots/js/main.js
@@ -5,10 +5,10 @@ function getDrink() {
   let drink = document.querySelector("input").value;
 }
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
   .then((res) => res.json()) // parse response as JSON
   .then((data) => {
-    console.log(data.drinks[0]);
+    console.log(data.drinks.forEach());
     document.querySelector("h2").innerText = data.drinks[0].strDrink;
     document.querySelector("img").src = data.drinks[0].strDrinkThumb;
     document.querySelector("h3").innerText = data.drinks[0].strInstructions;
--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/api-template/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/api-template/js/main.js
@@ -1,5 +1,3 @@
-//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
-
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
@@ -7,7 +5,7 @@ function getFetch(){
   const choice = document.querySelector('input').value
   const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
 
-  fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+  fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
