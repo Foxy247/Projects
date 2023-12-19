9d8ff094b7660d65f8983175fe2775a8037d09b6
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-12-19 17:12:08 +0100

    19

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-nasa/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class26-materials/objects-nasa/js/main.js
@@ -1,19 +1,37 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
 
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getDrink);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getDrink() {
+  let drink = document.querySelector("input").value;
 
-  fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+  fetch(`https://api.nasa.gov/`)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      document.querySelector("h2").innerText = data;
+      document.querySelector("img").src = data;
+      document.querySelector("h3").innerText = data;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
+
+class Person {
+  constructor(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    
+    walk(){
+      console.log('walking');
+    }
+    eat(){
+      console.log('eating');
+    }
+  }
+}
+
+
 
--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/objects-netflix-tv-shows/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,15 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class Netflix {
+    constructor(name,length,rating,views){
+        this.name = name
+        this.length = length
+        this.rating = rating
+        this.views = views
 
+        premier(){
+            console.log('this is a pemier');
+        }
+        
+    }
+}
