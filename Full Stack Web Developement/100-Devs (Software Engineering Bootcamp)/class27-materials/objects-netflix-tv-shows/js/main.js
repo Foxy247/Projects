a1118f7aa931d08a682d8d7c6dca8fa7fc498e39
EmanuelKoelbl <e.koelbl1@gmx.at>
2023-12-21 19:17:17 +0100

    21

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/objects-netflix-tv-shows/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,15 +1,17 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
 class Netflix {
-    constructor(name,length,rating,views){
-        this.name = name
-        this.length = length
-        this.rating = rating
-        this.views = views
+  constructor(name, length, rating, views) {
+    this.name = name;
+    this.length = length;
+    this.rating = rating;
+    this.views = views;
+  }
 
-        premier(){
-            console.log('this is a pemier');
-        }
-        
-    }
+  play() {
+    console.log("playing");
+  }
+  stop() {
+    console.log("stopping");
+  }
 }
