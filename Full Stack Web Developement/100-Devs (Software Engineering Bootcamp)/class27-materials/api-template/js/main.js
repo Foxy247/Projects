9532625cedbf3ea82681cb1798e8306190c85a89
EmanuelKoelbl <e.koelbl1@gmx.at>
2024-01-10 13:52:37 +0100

    10

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/api-template/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/api-template/js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector("input").value.toLowerCase();
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = "T8ccYLqA1zbcC2ChngTzOXNeGfYGDFSEmz0MTk6a" + choice;
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
@@ -15,4 +15,8 @@ function getFetch(){
       });
 }
 
+
+HhcbPKsveYR0MtmY3AOuDlxnQymQrnov2Ycb2jrt;
+
+// URL: T8ccYLqA1zbcC2ChngTzOXNeGfYGDFSEmz0MTk6a;
 
