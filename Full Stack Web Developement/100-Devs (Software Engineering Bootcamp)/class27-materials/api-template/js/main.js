9085ea09a519bf7d74805f5771379d6c8a161d1e
EmanuelKoelbl <e.koelbl1@gmx.at>
2024-01-13 16:18:40 +0100

    13

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/api-template/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/api-template/js/main.js
@@ -1,23 +1,24 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
+function getFetch() {
   const choice = document.querySelector("input").value.toLowerCase();
-  const url =
-    "https://api.nasa.gov/planetary/apod?api_key=T8ccYLqA1zbcC2ChngTzOXNeGfYGDFSEmz0MTk6a";
+
+  if (!choice) {
+    console.log("Please provide a valid choice");
+    return;
+  }
+
+  const apiKey = "HhcbPKsveYR0MtmY3AOuDlxnQymQrnov2Ycb2jrt";
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${choice}`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector("img").src = data.hdurl;
+      document.querySelector("h3").innerText = data.explanation;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
-
-
-// URL 1 HhcbPKsveYR0MtmY3AOuDlxnQymQrnov2Ycb2jrt;
-
-// URL: T8ccYLqA1zbcC2ChngTzOXNeGfYGDFSEmz0MTk6a;
-
