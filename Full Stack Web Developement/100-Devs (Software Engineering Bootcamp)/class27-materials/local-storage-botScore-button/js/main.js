66234d2126858b441e97801c1804137ff89939a6
EmanuelKoelbl <e.koelbl1@gmx.at>
2024-01-28 18:33:52 +0100

    28

--- Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/local-storage-botScore-button/js/main.js
+++ Full Stack Web Developement/100-Devs (Software Engineering Bootcamp)/class27-materials/local-storage-botScore-button/js/main.js
@@ -1,7 +1,11 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
-localStorage.setItem("botScore", 0);
+if (!localStorage) localStorage.setItem("botScore", 0);
 
 document.querySelector("button").addEventListener("click", addOne);
 
-function addOne() {}
+function addOne() {
+  let botScoreVal = Number(localStorage.getItem("botScore"));
+  botScoreVal += 1;
+  localStorage.setItem("botScore", botScoreVal);
+}
