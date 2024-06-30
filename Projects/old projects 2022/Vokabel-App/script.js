4ba74793440915abf7fde1fce31b31d57fb9e322
Foxy247 <83544812+Foxy247@users.noreply.github.com>
2024-06-30 11:01:08 +0200

    Rearranging

---
+++ Projects/old projects 2022/Vokabel-App/script.js
@@ -0,0 +1,12 @@
+let dictionary = {
+    "Hallo": "Hello",
+    "Apfel": "Apple",
+    "Friend": "Freund",
+    "Freundin": "Girlfriend",
+    "Programmiern": "Programming"
+
+}
+
+function addVocabulary(){
+    dictionary[germanText.value] = englishText.value;
+}
